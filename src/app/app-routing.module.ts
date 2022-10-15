31a3a00280eb2549609c88aa01f29e1200efa8aa
diananeg01 <diananegurita07@gmail.com>
2022-10-15 13:37:26 +0300

    change routing for detail-sign-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {DetailSignUpComponent} from "./sign-up/detail-sign-up/detail-sign-up.component";
+import {
+  PersonalInformationComponent
+} from "./sign-up/detail-sign-up/personal-information/personal-information.component";
+import {ConfirmComponent} from "./sign-up/detail-sign-up/confirm/confirm.component";
 
 const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
-  { path: 'sign-up/more', component: DetailSignUpComponent },
+  { path: 'sign-up-details', component: DetailSignUpComponent, children: [
+      { path: 'personal-information', component: PersonalInformationComponent },
+      { path: 'confirm', component: ConfirmComponent },
+      { path: '**', redirectTo: 'personal-information', pathMatch: 'full' }
+    ] },
   { path: '**', redirectTo: 'sign-up', pathMatch: 'full' },
   /*
   { path: 'sign-in', component: SignInComponent },
