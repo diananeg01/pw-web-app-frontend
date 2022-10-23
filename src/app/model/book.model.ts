b91b5b0418f1db18d9aab82e36657619f9a69f13
diananeg01 <diananegurita07@gmail.com>
2022-10-23 23:54:32 +0300

    modify models

--- src/app/model/book.model.ts
+++ src/app/model/book.model.ts
@@ -1,19 +1,9 @@
 export class BookModel {
-  id: string;
-  title: string;
-  author: string;
-  publishingHouse: string;
-  description: string;
-  category: string;
-  price: number;
-
-  constructor(id: string, title: string, author: string, publishingHouse: string, description: string, category: string, price: number) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.publishingHouse = publishingHouse;
-    this.description = description;
-    this.category = category;
-    this.price = price;
-  }
+  id?: string;
+  title?: string;
+  author?: string;
+  publishingHouse?: string;
+  description?: string;
+  category?: string;
+  price?: number;
 }
--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,21 +1,11 @@
 import {RoleEnum} from "./role.enum";
 
 export class UserModel {
-  id: string;
-  username: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-  password: string;
-  role: RoleEnum;
-
-  constructor(id: string, username: string, firstname: string, lastname: string, email: string, password: string) {
-    this.id = id;
-    this.username = username;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
-    this.password = password;
-    this.role = RoleEnum.READER;
-  }
+  id?: string;
+  username?: string;
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+  role?: RoleEnum;
 }
