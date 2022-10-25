0bca99a5d133469a2008d773f3aa7e921a226218
diananeg01 <diananegurita07@gmail.com>
2022-10-25 16:23:37 +0300

    create add visual components to library-app and update routing

---
+++ src/app/library-app/library-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-library-app',
+  templateUrl: './library-app.component.html',
+  styleUrls: ['./library-app.component.css']
+})
+export class LibraryAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
